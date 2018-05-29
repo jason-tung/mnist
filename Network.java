010039ca39fd8cf6fd4c1075000bc3ffd3eb631d
Jason <mickeu420@gmail.com>
2018-05-29 17:00:13 -0400

    skeletonfile

---
+++ Network.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class Network{
+    LossFunction loss_function;
+    Layer[] layers;
+    int count_layers;
+    int count_nodes;
+    String descent_mode;
+
+    public Network(int numLayers, int numNodes, Activation activFunc, LossFunction lossFunc){
+
+    }
+    
+    //descent
+    public void fit(){
+	
+    }
+
+    //takes in file name and predicts number
+    public int predict(String fileName);
+
+    
+}
+
+class Layer{
+    
+}
