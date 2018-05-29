d6cc39d544166f334a03b49b7c161fd91694fdbe
Jason <mickeu420@gmail.com>
2018-05-29 16:04:59 -0400

    skeleton for preprocessing is complete

--- Preprocessing.java
+++ Preprocessing.java
@@ -1,9 +1,20 @@
 import java.util.*;
 
 public class Preprocessing{
-    public static array parse(String){
-    }
-	public static array flatten(){
+    
+    //returns [label, matrix] for picture
+    public static Object[]  parse(String){
 	
     }
+    
+    //creates an input layer
+    public static array flatten(array){
+	
+    }
+    
+    //creates the one-hot layer with which to compare output layer
+    public static array to_categorical(array){
+	
+    }
+    
 }
