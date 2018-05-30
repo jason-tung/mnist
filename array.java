538a034f2b3909c58a0cc0816604a82c51467d58
Alwin Peng <alwinpeng@live.com>
2018-05-30 15:35:43 -0400

    usefull things

--- array.java
+++ array.java
@@ -1,19 +1,34 @@
 import java.util.ArrayList;
 
-public class array extends ArrayList<Object>{
+public class tensor{
     int[] shape;
     //ensure that all these arrays are rectangular
-
-
-    private void recursive_parse(Object T){
-        while(!(T instanceof Double)){
-            for(Object i: (Iterable<Object>) T){
-                recursive_parse(i);
-            }
-        }
+    //ensure that the dtype of all elements is constant
+    public array(int[] shape){
+	ArrayList<Object> data = new ArrayList<Object>();
+	this.shape = shape;
+	construct(data, this.shape, 0);
     }
 
-    public array(Object T){
+    private int len(int[] obj){return obj.size();}
 
+    private void construct(ArrayList<Object> current_obj, int[] shape, int pos){
+
+	int val = shape[pos];
+	
+	if(pos == len(shape)-1){
+	    for(int i = 0; i < val; i++){
+		current_obj.add(Double.valueOf(0));
+	    }
+	} else{
+	    for(i = 0; i < val; i++){
+		ArrayList<Object> nxt = new ArrayList<Object>();
+		current_obj.add(nxt);
+		construct(nxt, shape, pos+1);
+	    }
+	}
+			
     }
+
+    public Object i
 }
