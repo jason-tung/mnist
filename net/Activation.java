6ffd64dac6e681c500b27a4572e2154c0aa3a41b
Jason <mickeu420@gmail.com>
2018-06-01 15:30:26 -0400

    finished subtract

--- net/Activation.java
+++ net/Activation.java
@@ -38,23 +38,7 @@ public class Activation {
 
     }
 
-    private static tensor rowMaxs(tensor x) {
-        int[] f = {x.shape[0]};
-        tensor rTensor = tensor.zeros(f);
-        for (int i = 0; i < x.shape[0]; i++) {
-            int[] g = {i, 0};
-            Double max = x.get(g).data;
-            for (int j = 1; j <= x.shape[1]; j++) {
-                int[] h = {i, j};
-                if (x.get(h).data > max) {
-                    max = x.get(h).data;
-                }
-            }
-            int[] j = {i};
-            rTensor.set(j, max);
-        }
-        return rTensor;
-    }
+    
 
     private static double sigmoid(double x) {
         return 1 / (1 + Math.exp(-x));
--- np/np.java
+++ np/np.java
@@ -1,3 +1,4 @@
+import java.util.*;
 package np;
 
 public class np {
@@ -9,7 +10,48 @@ public class np {
 
     public static boolean equal(tensor arr1, tensor arr2){return true;}
 
+    
 //    public static tensor matmul(tensor arr){};
+    
+    public static tensor rowMaxs(tensor x) {
+        int[] f = {x.shape[0]};
+        tensor rTensor = tensor.zeros(f);
+        for (int i = 0; i < x.shape[0]; i++) {
+            int[] g = {i, 0};
+            Double max = x.get(g).data;
+            for (int j = 1; j <= x.shape[1]; j++) {
+                int[] h = {i, j};
+                if (x.get(h).data > max) {
+                    max = x.get(h).data;
+                }
+            }
+            int[] j = {i};
+            rTensor.set(j, max);
+        }
+        return rTensor;
+    }
+
+    //only deals with 2d arrays for now i dont know what funky stuff you did for n dimensional arrays
+    public static tensor subtract(tensor a, tensor b){
+	if (Arrays.equals(a.shape, b.shape)){
+	    int[] shape = a.shape;
+	    tensor rTensor = tensor.zeros(shape);
+	    for (int i = 0; i < shape[0]; i++){
+		for (int j = 0; j < shape[1]; j++){
+		    int[] currentLoc = {i,j};
+		    rTensor.set(currentLoc, a.get(currentLoc).data - b.get(currentLoc).data);
+		    
+		}
+	    }
+	    return rTensor;
+	}
+	throw new IllegalArgumentException("tensors of different shapes");
+    }
+
+    public static tensor exp(tensor a){
+	
+    }
+
 
 
 
